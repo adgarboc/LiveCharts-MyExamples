dbedf58be41eb533c2f02b58e9413e7e176b710d
adgarboc <adgarboc@gmx.com>
2020-05-02 15:49:44 -0500

    Default example

--- LiveCharts-MyExamples/MainWindow.xaml.cs
+++ LiveCharts-MyExamples/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 namespace LiveCharts_MyExamples
 {
@@ -23,6 +26,38 @@ namespace LiveCharts_MyExamples
         public MainWindow()
         {
             InitializeComponent();
+
+            var r = new Random();
+            ValuesA = new ChartValues<ObservablePoint>();
+            ValuesB = new ChartValues<ObservablePoint>();
+            ValuesC = new ChartValues<ObservablePoint>();
+
+            for (var i = 0; i < 20; i++)
+            {
+                ValuesA.Add(new ObservablePoint(r.NextDouble() * 10, r.NextDouble() * 10));
+                ValuesB.Add(new ObservablePoint(r.NextDouble() * 10, r.NextDouble() * 10));
+                ValuesC.Add(new ObservablePoint(r.NextDouble() * 10, r.NextDouble() * 10));
+            }
+
+            DataContext = this;
+        }
+
+        public ChartValues<ObservablePoint> ValuesA { get; set; }
+        public ChartValues<ObservablePoint> ValuesB { get; set; }
+        public ChartValues<ObservablePoint> ValuesC { get; set; }
+
+        private void RandomizeOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+            for (var i = 0; i < 20; i++)
+            {
+                ValuesA[i].X = r.NextDouble() * 10;
+                ValuesA[i].Y = r.NextDouble() * 10;
+                ValuesB[i].X = r.NextDouble() * 10;
+                ValuesB[i].Y = r.NextDouble() * 10;
+                ValuesC[i].X = r.NextDouble() * 10;
+                ValuesC[i].Y = r.NextDouble() * 10;
+            }
         }
     }
 }
